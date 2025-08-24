065b3a08dcd0fedd271a6ab9e17cc7c3d5314f87
SaidPM <said2005marquez@gmail.com>
2025-08-23 21:27:26 -0500

    estados creados y los eventos

--- blocu/lib/presentation/bloc/princi_bloc_state.dart
+++ blocu/lib/presentation/bloc/princi_bloc_state.dart
@@ -1,6 +1,15 @@
 part of 'princi_bloc_bloc.dart';
 
-@immutable
-sealed class PrinciBlocState {}
 
-final class PrinciBlocInitial extends PrinciBlocState {}
+sealed class PrinciBlocState extends Equatable {
+  const PrinciBlocState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class BlocInitial extends PrinciBlocState {}
+class BlocLoading extends PrinciBlocState{}
+class BlocSucces extends PrinciBlocState{}
+class BlocFailure extends PrinciBlocState{}
+
---
+++ blocu/lib/presentation/pages/vistage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InicialVista extends StatelessWidget{
+  const InicialVista({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
