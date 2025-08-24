a30f8defd22cb9a290affbf1864859419bdfddaf
SaidPM <said2005marquez@gmail.com>
2025-08-23 22:10:40 -0500

    eventos junto con peticion hechos

--- blocu/lib/presentation/bloc/princi_bloc_bloc.dart
+++ blocu/lib/presentation/bloc/princi_bloc_bloc.dart
@@ -1,15 +1,43 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 part 'princi_bloc_event.dart';
 part 'princi_bloc_state.dart';
 
 class PrinciBlocBloc extends Bloc<PrinciBlocEvent, PrinciBlocState> {
-  PrinciBlocBloc() : super(BlocInitial()) {
-    on<PrinciBlocEvent>((event, emit) {
-      // TODO: implement event handler
+  Timer? tiempo;
+  PrinciBlocBloc() : super(BlocInitial()){
+  on<Cargarprofe>(pretiprof);
+  //aqui agrego un timer que actualiza el widget
+  add(Cargarprofe());
+    tiempo = Timer.periodic(const Duration(seconds: 15), (_) {
+      add(Cargarprofe());
     });
   }
+  Future<void> pretiprof( Cargarprofe event, Emitter <PrinciBlocState> emit) async{
+    emit(BlocLoading());
+    try{
+      final url = Uri.parse('https://mocki.io/v1/fa975218-53c3-4a60-81a2-5c3cd23f3f89');
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final List<String> profesores = (data["profesores"] as List)
+            .map((p) => p["nombre"].toString())
+            .toList();
+
+        emit(BlocSucces(profesores) );
+      } else {
+        emit(BlocFailure("Error: ${response.statusCode}") );
+      }
+    } catch (e) {
+      emit(BlocFailure("Error al cargar profesores "));
+    }
+        
+  }
+ 
 }
--- blocu/lib/presentation/pages/vistage.dart
+++ blocu/lib/presentation/pages/vistage.dart
@@ -1,11 +1,22 @@
+import 'package:blocu/presentation/bloc/princi_bloc_bloc.dart';
+import 'package:blocu/presentation/cubit/princi_cubit_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class InicialVista extends StatelessWidget{
+
+
+class InicialVista extends StatelessWidget {
   const InicialVista({super.key});
-
+  
   
   @override
   Widget build(BuildContext context) {
-    
+    final cubit = context.read<PrinciCubitCubit>();
+
+    return Scaffold( backgroundColor: Colors.grey,
+      appBar: AppBar(centerTitle: true,title: Text("Bloc/Cubit prueba", style: TextStyle(color: Colors.white),), backgroundColor: Colors.black),
+      
+    );
+
   }
 }
