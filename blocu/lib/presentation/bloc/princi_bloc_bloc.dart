3615bedc4b1393b7bd4cef5593745dc54bd2da74
SaidPM <said2005marquez@gmail.com>
2025-08-25 08:03:55 -0500

    hecho

--- blocu/lib/presentation/bloc/princi_bloc_bloc.dart
+++ blocu/lib/presentation/bloc/princi_bloc_bloc.dart
@@ -5,24 +5,23 @@ import 'package:equatable/equatable.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
 part 'princi_bloc_event.dart';
 part 'princi_bloc_state.dart';
 
 class PrinciBlocBloc extends Bloc<PrinciBlocEvent, PrinciBlocState> {
-  Timer? tiempo;
-  PrinciBlocBloc() : super(BlocInitial()){
-  on<Cargarprofe>(pretiprof);
-  //aqui agrego un timer que actualiza el widget
-  add(Cargarprofe());
-    tiempo = Timer.periodic(const Duration(seconds: 15), (_) {
-      add(Cargarprofe());
-    });
+  PrinciBlocBloc() : super(BlocInitial()) {
+    on<Cargarprofe>(pretiprof);
+    add(Cargarprofe());
   }
-  Future<void> pretiprof( Cargarprofe event, Emitter <PrinciBlocState> emit) async{
+  Future<void> pretiprof(
+    Cargarprofe event,
+    Emitter<PrinciBlocState> emit,
+  ) async {
     emit(BlocLoading());
-    try{
-      final url = Uri.parse('https://mocki.io/v1/fa975218-53c3-4a60-81a2-5c3cd23f3f89');
+    try {
+      final url = Uri.parse(
+        'https://mocki.io/v1/fa975218-53c3-4a60-81a2-5c3cd23f3f89',
+      );
       final response = await http.get(url);
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
@@ -30,14 +29,12 @@ class PrinciBlocBloc extends Bloc<PrinciBlocEvent, PrinciBlocState> {
             .map((p) => p["nombre"].toString())
             .toList();
 
-        emit(BlocSucces(profesores) );
+        emit(BlocSucces(profesores));
       } else {
-        emit(BlocFailure("Error: ${response.statusCode}") );
+        emit(BlocFailure("Error: ${response.statusCode}"));
       }
     } catch (e) {
       emit(BlocFailure("Error al cargar profesores "));
     }
-        
   }
- 
 }
